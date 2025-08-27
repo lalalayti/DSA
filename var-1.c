f2d355c51a275e6fc5b36ad538c036bd8f3fec9a
DSA <lightpearlegaspi@gmail.com>
2025-08-27 19:39:06 +0800

    changes made

--- var-1.c
+++ var-1.c
@@ -7,22 +7,21 @@ typedef struct {
     int count;
 } List;
 
-List initialize(List L){
+List initialize(List L) {
     L.count = 0;
     return L;
 }
 
-List insertPos(List L, int data, int position){
-    if(position > L.count || position == L.count){
-        printf("Invalid position.");
+List insertPos(List L, int data, int position) {
+    if (position<0 || position>L.count) {
+        printf("Invalid position.\n");
         exit(1);
     }
-    if(L.count==MAX){
-        printf("Array is full.");
+    if (L.count==MAX) {
+        printf("Array is full.\n");
         exit(1);
     }
-
-    for(int i=L.count;i>position;i--){
+    for (int i=L.count;i>position;i--) {
         L.elem[i]=L.elem[i-1];
     }
     L.elem[position]=data;
@@ -30,39 +29,63 @@ List insertPos(List L, int data, int position){
     return L;
 }
 
-List deletePos(List L, int position){
-    if(position > L.count || position == L.count){
-        printf("Invalid position.");
+List deletePos(List L, int position) {
+    if (position<0 || position>=L.count) {
+        printf("Invalid position.\n");
         exit(1);
     }
-    for(int i=position;i<L.count-1;i++){
-        L.elem[i]=L.elem[i-1];
+    for (int i=position;i<L.count-1;i++) {
+        L.elem[i]=L.elem[i+1];
     }
     L.count--;
     return L;
 }
 
-int locate(List L, int data){
-    for(int i=0;i<L.count;i++){
-        if(L.elem[i]==data){
+int locate(List L, int data) {
+    for (int i=0;i<L.count;i++) {
+        if (L.elem[i]==data) {
             return i;
-        } else {
-            return -1;
         }
     }
+    return -1;
 }
 
-int main(){
+List insertSorted(List L, int data) {
+    int i=0;
+    if (L.count==MAX) {
+        printf("Array is full.\n");
+        exit(1);
+    }
+    for (i=L.count-1;i>=0 && L.elem[i]>data;i--) {
+        L.elem[i+1]=L.elem[i];
+    }
+    L.elem[i+1]=data;
+    L.count++;
+    return L;
+}
+
+
+void display(List L) {
+    for (int i=0;i<L.count;i++) {
+        printf("%d ", L.elem[i]);
+    }
+    printf("\n");
+}
+
+int main() {
     List L;
-    List init = initialize(L);
-    int selected;
-    int val;
-    int pos;
+    L = initialize(L);
+
+    int selected, val, pos;
 
     printf("Enter size of array: ");
     scanf("%d", &L.count);
+    if (L.count > MAX) {
+        printf("Invalid size.\n");
+        return 1;
+    }
     printf("Enter values: ");
-    for(int i=0;i<L.count;i++){
+    for (int i=0;i<L.count;i++) {
         scanf("%d", &L.elem[i]);
     }
 
@@ -74,41 +97,47 @@ int main(){
     printf("\nEnter operation: ");
     scanf("%d", &selected);
 
-    List newArr;
-
-    switch(selected){
+    switch (selected) {
         case 1:
-        printf("Enter position to insert: ");
-        scanf("%d", &pos);
-        printf("Enter value: ");
-        scanf("%d", &val);
-        newArr = insertPos(L, val, pos);
-        printf("Updated array: ");
-        for(int i=0;i<newArr.count;i++){
-            printf("%d ", newArr.elem[i]);
-        }
-        break;
-        
-        case 2:
-        printf("Enter position to delete: ");
-        scanf("%d", &pos);
-        newArr = deletePos(L, pos);
-        printf("Updated array: ");
-        for(int i=0;i<newArr.count;i++){
-            printf("%d ", newArr.elem[i]);
-        }
-        break;
-        case 3:
-        printf("Enter value to locate: ");
-        scanf("%d", &val);
-        int returned = locate(L, val);
-        if(returned == -1){
-            printf("Value can't be found.");
-        } else {
-            printf("Value %d is at position %d.", val, returned);
-        }
-        
+            printf("Enter position to insert: ");
+            scanf("%d", &pos);
+            printf("Enter value: ");
+            scanf("%d", &val);
+            L = insertPos(L, val, pos);
+            display(L);
+            break;
 
+        case 2:
+            printf("Enter position to delete: ");
+            scanf("%d", &pos);
+            L = deletePos(L, pos);
+            display(L);
+            break;
+
+        case 3:
+            printf("Enter value to locate: ");
+            scanf("%d", &val);
+            pos = locate(L, val);
+            if (pos == -1) {
+                printf("Value not found.\n");
+            } else {
+                printf("Value %d is at position %d.\n", val, pos);
+            }
+            break;
+
+        case 4:
+            printf("Enter value to insert sorted: ");
+            scanf("%d", &val);
+            L = insertSorted(L, val);
+            display(L);
+            break;
+
+        case 5:
+            display(L);
+            break;
+
+        default:
+            printf("Invalid choice.\n");
     }
     return 0;
 }
